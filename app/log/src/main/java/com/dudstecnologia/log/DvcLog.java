374b73ba14e7842ede2af9a0c1e5b6c5023d2746
Eduardo Silva <eduardo@dudstecnologia.com.br>
2023-05-27 10:21:02 -0400

    New Module

---
+++ app/log/src/main/java/com/dudstecnologia/log/DvcLog.java
@@ -0,0 +1,14 @@
+package com.dudstecnologia.log;
+
+import android.util.Log;
+
+public class DvcLog {
+
+    public void log1(String msg) {
+        Log.d("DVC_LOG", "Log 1: " + msg);
+    }
+
+    public void log2(String msg) {
+        Log.d("DVC_LOG", "Log 2: " + msg);
+    }
+}
